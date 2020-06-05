eef62aa0f38bfc81ca92a2ef3a8779d3346488cd
James Brunet <jamesbrunet@live.ca>
2020-06-05 18:00:49 -0400

    Add get_agent_performance function
    Adds a get_agent_performance function that shows agent activity in a
    given timeframe. Some sample code is also present that calls this
    function: API key not included!

---
+++ getleaderboard.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+def get_agent_performance(start_date,end_date, api_key):
+    
+    head = {"Authorization":"Token " + api_key}
+    param = {"start_date":start_date,"end_date":end_date}
+
+    r_van = requests.get("https://api.callhub.io/v1/analytics/agent-leaderboard", headers=h, params=p)
+
+    agents = r_van.json().get("plot_data")
+    
+    print(agents)
+    return agents
+
+
+api_key='YOUR API KEY HERE'
+get_agent_performance("2019-12-01","2020-12-30", api_key)
